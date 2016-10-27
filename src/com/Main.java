42923e5f4a266c08496b97ea047702eb775e5c9f
shevtsod <daniel.shevtsov@gmail.com>
2016-10-26 19:33:37 -0600

    Create src/ folder, stub for Main, package-info for package shevtsod.

---
+++ src/com/Main.java
@@ -0,0 +1,18 @@
+/*
+ * FILENAME:        Main.java
+ * COURSE:          ENSE 374
+ * AUTHOR:          Daniel Shevtsov
+ */
+
+package com;
+
+/**
+ * class Main
+ *
+ * Serves as primary entry point for program.
+ *
+ * @author Daniel Shevtsov
+ */
+public class Main {
+    //TODO: Add main body
+}
---
+++ src/com/shevtsod/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <h1>shevtsod</h1>
+ * Contains implementations for the following classes:
+ * <ul>
+ *     TODO: Add class names here
+ * </ul>
+ * @author Daniel Shevtsov
+ */
+package com.shevtsod;
+
+//Empty file -- Purpose: define Javadoc for package "shevtsod".
